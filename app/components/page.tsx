b4ab44541b87f3ce5c0fe45d86ab2f425ee781a1
선영 <tjsduddlfma1@naver.com>
2025-11-17 18:44:26 +0900

    💄 style: 버튼 컴포넌트 스타일 설정

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -4,7 +4,13 @@ export default function ComponentsAll() {
   return (
     <div>
       <h1>컴포넌트 확인용 페이지</h1>
-      <Button/>
+      
+      <section className='flex gap-4 items-center'>
+        <Button>버튼</Button>
+        <Button variant="light">버튼</Button>
+        <Button variant="secondaryLight">버튼</Button>
+        <Button disabled>버튼</Button>
+      </section>
     </div>
   );
 }
--- components/button.tsx
+++ components/button.tsx
@@ -1,3 +1,47 @@
-export default function Button() {
-  return <button>클릭</button>
+import clsx from "clsx";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?:'default'|'light'|'secondaryLight';
+  disabled?:boolean;
+  children:React.ReactNode;
+}
+
+export default function Button({
+  variant='default',
+  disabled=false,
+  children,
+  ...restProps
+}:ButtonProps) {
+
+  // 기본 스타일
+  const baseStyles = "relative px-4 py-3 rounded-sm min-w-20 overflow-hidden focus:outline-(--color-dev-fuchsia)"
+
+  // 타입별 배경색
+  const typeStyles = {
+    default : "bg-(--color-dev-primary) text-white cursor-pointer",
+    light: "bg-(--color-dev-primary200) text-(--color-dev-primary) cursor-pointer",
+    secondaryLight: "bg-(--color-dev-primary300) text-(--color-dev-primary) cursor-pointer",
+  }
+
+  // 상태별 제어(disabled)
+  const statusStyles = disabled ? "cursor-not-allowed text-(--color-dev-gray300) bg-(--color-dev-gray400)" : ""
+  
+  return (
+    <button className={clsx(
+        baseStyles, 
+        !disabled && typeStyles[variant],  
+        statusStyles
+      )} disabled={disabled} {...restProps}>
+
+      {/* 텍스트 */}
+      <span className="relative z-10">{children}</span>
+
+      {/* hover 오버레이 */}
+      {
+        !disabled && (
+          <span className="absolute inset-0 bg-black opacity-0 hover:opacity-10 transition-opacity duration-300"></span>
+        )
+      }
+    </button>
+  );
 }
