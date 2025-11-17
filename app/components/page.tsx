8221f7f0de55b4f1da2793c87ef0914d9335e6c6
선영 <tjsduddlfma1@naver.com>
2025-11-17 17:26:28 +0900

    🧪 test: 버튼 컴포넌트 확인용 제작

---
+++ app/components/page.tsx
@@ -0,0 +1,10 @@
+import Button from '@/components/button';
+
+export default function ComponentsAll() {
+  return (
+    <div>
+      <h1>컴포넌트 확인용 페이지</h1>
+      <Button/>
+    </div>
+  );
+}
---
+++ components/button.tsx
@@ -0,0 +1,3 @@
+export default function Button() {
+  return <button>클릭</button>
+}
