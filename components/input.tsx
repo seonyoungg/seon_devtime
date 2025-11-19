f7ef403ef6365270622cbd56e15f80ddb0f4d251
선영 <tjsduddlfma1@naver.com>
2025-11-19 14:46:02 +0900

    💄 style: 인풋 컴포넌트 스타일 조정

--- components/input.tsx
+++ components/input.tsx
@@ -1,7 +1,35 @@
+'use client';
+
+import clsx from "clsx";
+import { useState } from "react";
+
 export default function InputItem(){
+  const [value, setValue] = useState("");
+  const handleChange =  (e:React.ChangeEvent<HTMLInputElement>)=>{
+    const val = e.target.value;
+
+    if (val === "" || val.trim() !== "") {
+      setValue(val);
+    }
+  }
+
+
   return (
-    <>
-      <input type="text" placeholder="input placeholder" />
-    </>
+    <div className="group flex items-center gap-2  py-4 px-6 rounded-md bg-(--color-dev-gray100)">
+      <input 
+        type="text" 
+        placeholder="Placeholder" 
+        className={clsx(
+          "font-medium flex-1 bg-transparent outline-none group-hover:text-(--color-dev-gray800)",
+          value ? "text-(--color-dev-gray600)" : "text-(--color-dev-gray300)"
+        )} 
+        onChange={handleChange} 
+        value={value}/>
+      <button className={clsx(
+        "font-bold group-hover:text-(--color-dev-primary)",
+        value ? "text-(--color-dev-primary) cursor-pointer" : "text-(--color-dev-gray400)"
+      )}>추가</button>
+    </div>
+
   )
 }
