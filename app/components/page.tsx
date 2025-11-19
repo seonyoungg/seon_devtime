66ca1412014d13a0e4843dc348e258e0b1ca00c9
선영 <tjsduddlfma1@naver.com>
2025-11-19 16:38:01 +0900

    💄 style: 모달 알림창 컴포넌트 생성

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,10 +1,11 @@
+import AlertModal from '@/components/alertModal';
 import Button from '@/components/button';
 import ImgUploader from '@/components/imgUploader';
 import InputItem from '@/components/input';
 
 export default function ComponentsAll() {
   return (
-    <div>
+    <div className='p-8'>
       <h1>컴포넌트 확인용 페이지</h1>
       
       <section className='space-y-4 my-8'>
@@ -26,6 +27,13 @@ export default function ComponentsAll() {
         <h2>이미지 업로드 컴포넌트</h2>
         <div className='flex flex-col gap-4'>
           <ImgUploader/>
+        </div>
+      </section>
+      <section className='space-y-4 my-8'>
+        <h2>모달 알림창 컴포넌트</h2>
+        <div className='flex flex-col gap-4'>
+          <AlertModal title="제목" content="내용"/>
+          <AlertModal content="내용만 있는경우"/>
         </div>
       </section>
     </div>
