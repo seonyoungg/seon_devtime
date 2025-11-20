1e9a5f7eb6199da1ed90e6f0aaf837aa69446a7a
seonyoungg <tjsduddlfma1@naver.com>
2025-11-20 21:13:35 +0900

    🧪 test: 테일윈즈 속성 사용 관련 테스트

--- components/alertModal.tsx
+++ components/alertModal.tsx
@@ -1,25 +1,21 @@
 import Button from '@/components/button';
 
-interface AlertModalProps{
-  title? : string;
-  content : string;
+interface AlertModalProps {
+  title?: string;
+  content: string;
 }
 
-export default function AlertModal({title,content}:AlertModalProps) {
+export default function AlertModal({ title, content }: AlertModalProps) {
   return (
-    <div className="flex flex-col justify-between gap-6 w-80 p-6 bg-white rounded-lg shadow-xl shadow-gray-600/20">
+    <div className='flex flex-col justify-between gap-6 w-80 p-6 bg-white rounded-lg shadow-xl shadow-gray-600/20'>
       <div className='space-y-2'>
-        {
-          title && (
-            <p className='text-xl font-semibold text-(--color-dev-gray800)'>{title}</p>
-          )
-        }
-        <p className='font-medium text-(--color-dev-gray500)'>{content}</p>
+        {title && <p className='text-xl font-semibold text-dev-gray800'>{title}</p>}
+        <p className='font-medium text-dev-gray500'>{content}</p>
       </div>
       <div className='flex gap-4 items-center justify-end w-full'>
-        <Button variant="secondaryLight">버튼</Button>
+        <Button variant='secondaryLight'>버튼</Button>
         <Button>버튼</Button>
       </div>
     </div>
-  )
+  );
 }
