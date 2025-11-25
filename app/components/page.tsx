eabd3b12a87add96f1d22906f6fa8922029c0a06
seonyoungg <tjsduddlfma1@naver.com>
2025-11-25 23:36:32 +0900

    💄 style: 코드 Prettier 포맷팅 적용

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -8,39 +8,39 @@ export default function ComponentsAll() {
   return (
     <div className='p-8'>
       <h1>컴포넌트 확인용 페이지</h1>
-      
+
       <section className='space-y-4 my-8'>
         <h2>버튼 컴포넌트</h2>
         <div className='flex gap-4 items-center'>
           <Button>버튼</Button>
-          <Button variant="light">버튼</Button>
-          <Button variant="secondaryLight">버튼</Button>
+          <Button variant='light'>버튼</Button>
+          <Button variant='secondaryLight'>버튼</Button>
           <Button disabled>버튼</Button>
         </div>
       </section>
       <section className='space-y-4 my-8'>
         <h2>인풋 컴포넌트</h2>
         <div className='flex gap-4 items-center'>
-          <InputItem/>
+          <InputItem />
         </div>
       </section>
       <section className='space-y-4 my-8'>
         <h2>이미지 업로드 컴포넌트</h2>
         <div className='flex flex-col gap-4'>
-          <ImgUploader/>
+          <ImgUploader />
         </div>
       </section>
       <section className='space-y-4 my-8'>
         <h2>모달 알림창 컴포넌트</h2>
         <div className='flex flex-col gap-4'>
-          <AlertModal title="제목" content="내용"/>
-          <AlertModal content="내용만 있는경우"/>
+          <AlertModal title='제목' content='내용' />
+          <AlertModal content='내용만 있는경우' />
         </div>
       </section>
       <section className='space-y-4 my-8'>
         <h2>랭킹 아이템 컴포넌트</h2>
         <div className='flex flex-col gap-4'>
-          <RankingItem/>
+          <RankingItem />
         </div>
       </section>
     </div>
