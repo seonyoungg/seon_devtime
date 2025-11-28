2517a51cd8c93b050c100e460650a0c58b97b6b1
seonyoungg <tjsduddlfma1@naver.com>
2025-11-29 02:17:29 +0900

    💄 style: 타이머 컴포넌트 반응형 작업

--- components/timerItem.tsx
+++ components/timerItem.tsx
@@ -5,9 +5,9 @@ interface TimerBoxProps {
 
 export default function TimerItem({ value = 0, label }: TimerBoxProps) {
   return (
-    <div className='p-4 pb-8 border border-dev-primary text-dev-primary rounded-xl bg-linear-to-br from-dev-primary/0 to-dev-primary/20 w-[220px]'>
-      <p className='text-[154px] font-digital tracking-widest text-center'>{value ? String(value).padStart(2, '0') : '00'}</p>
-      <p className='text-sm font-semibold text-center'>{label}</p>
+    <div className='w-full col-span-3 p-4 pb-8 border border-dev-primary text-dev-primary rounded-xl bg-linear-to-br from-dev-primary/0 to-dev-primary/20'>
+      <p className='text-[15vw] lg:text-[154px] font-digital tracking-widest text-center'>{value ? String(value).padStart(2, '0') : '00'}</p>
+      <p className='text-[12px] xs:text-sm font-semibold text-center'>{label}</p>
     </div>
   );
 }
