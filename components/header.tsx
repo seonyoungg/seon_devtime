61b3c3c9197e96be228526632a215fb293037da0
seonyoungg <tjsduddlfma1@naver.com>
2025-11-28 22:39:28 +0900

    💄 style: 헤더 컴포넌트 생성

---
+++ components/header.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function DevHeader() {
+  return (
+    <header className='py-4 px-6 bg-white border-b border-dev-gray200'>
+      <div className='flex items-center gap-12'>
+        <Link href='/' className='text-xl font-bold text-dev-primary'>
+          <Image src='/logo/logo_horizen_brand.svg' width={148} height={40} alt='로고이미지' />
+        </Link>
+
+        <nav className='flex items-center justify-between flex-1'>
+          <ul className='flex items-center gap-9'>
+            <li>
+              <Link href='/dashboard' className='text-dev-secondary font-semibold hover:underline underline-offset-4'>
+                대시보드
+              </Link>
+            </li>
+            <li>
+              <Link href='/rank' className='text-dev-secondary font-semibold hover:underline underline-offset-4'>
+                랭킹
+              </Link>
+            </li>
+            <li>
+              <Link href='/components' className='text-dev-secondary font-semibold hover:underline underline-offset-4'>
+                컴포넌트
+              </Link>
+            </li>
+          </ul>
+
+          <ul className='flex items-center gap-8'>
+            <li>
+              <Link href='/login' className='text-dev-secondary font-semibold hover:underline underline-offset-4'>
+                로그인
+              </Link>
+            </li>
+            <li>
+              <Link href='/join' className='text-dev-secondary font-semibold hover:underline underline-offset-4'>
+                회원가입
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
