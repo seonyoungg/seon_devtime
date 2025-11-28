e7b1a0d308bf6c005eb03b63b2548c71817e2f0c
seonyoungg <tjsduddlfma1@naver.com>
2025-11-28 22:48:28 +0900

    💄 style: 레이아웃 내 헤더 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { ToastContainer } from 'react-toastify';
+import DevHeader from '@/components/header';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className='font-sans'>
+        <DevHeader />
         {children}
         <ToastContainer position='top-center' autoClose={2000} hideProgressBar closeOnClick pauseOnHover draggable />
       </body>
