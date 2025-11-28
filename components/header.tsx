5916c264285c9542c518c5b8ab0a0f7a8320a2c7
seonyoungg <tjsduddlfma1@naver.com>
2025-11-29 02:17:11 +0900

    💄 style: 헤더 반응형 추가 작업

--- components/header.tsx
+++ components/header.tsx
@@ -20,7 +20,7 @@ export default function DevHeader() {
   ];
 
   return (
-    <header className='text-sm xs:text-[16px] fixed top-0 left-0 w-full px-6 py-4 bg-white border-b border-dev-gray200 z-10000'>
+    <header className='text-sm xs:text-[16px] fixed top-0 left-0 w-full px-2 2xs:px-6 py-4 bg-white border-b border-dev-gray200 z-10000'>
       <div className='flex items-center gap-4 sm:gap-12'>
         <Link href='/' className='shrink'>
           <Image src='/logo/logo_horizen_brand.svg' width={148} height={40} alt='로고이미지' className='h-auto w-28 sm:w-37' />
