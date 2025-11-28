e48d0b4d905a79a2832874e53f6fa90a2c24a727
seonyoungg <tjsduddlfma1@naver.com>
2025-11-28 22:41:13 +0900

    💄 style: 헤더 포지션 고정 및 headwind 포맷팅 적용

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function DevHeader() {
   return (
-    <header className='py-4 px-6 bg-white border-b border-dev-gray200'>
+    <header className='fixed top-0 left-0 w-full px-6 py-4 bg-white border-b border-dev-gray200 z-10000'>
       <div className='flex items-center gap-12'>
         <Link href='/' className='text-xl font-bold text-dev-primary'>
           <Image src='/logo/logo_horizen_brand.svg' width={148} height={40} alt='로고이미지' />
@@ -12,17 +12,17 @@ export default function DevHeader() {
         <nav className='flex items-center justify-between flex-1'>
           <ul className='flex items-center gap-9'>
             <li>
-              <Link href='/dashboard' className='text-dev-secondary font-semibold hover:underline underline-offset-4'>
+              <Link href='/dashboard' className='font-semibold text-dev-secondary hover:underline underline-offset-4'>
                 대시보드
               </Link>
             </li>
             <li>
-              <Link href='/rank' className='text-dev-secondary font-semibold hover:underline underline-offset-4'>
+              <Link href='/rank' className='font-semibold text-dev-secondary hover:underline underline-offset-4'>
                 랭킹
               </Link>
             </li>
             <li>
-              <Link href='/components' className='text-dev-secondary font-semibold hover:underline underline-offset-4'>
+              <Link href='/components' className='font-semibold text-dev-secondary hover:underline underline-offset-4'>
                 컴포넌트
               </Link>
             </li>
@@ -30,12 +30,12 @@ export default function DevHeader() {
 
           <ul className='flex items-center gap-8'>
             <li>
-              <Link href='/login' className='text-dev-secondary font-semibold hover:underline underline-offset-4'>
+              <Link href='/login' className='font-semibold text-dev-secondary hover:underline underline-offset-4'>
                 로그인
               </Link>
             </li>
             <li>
-              <Link href='/join' className='text-dev-secondary font-semibold hover:underline underline-offset-4'>
+              <Link href='/join' className='font-semibold text-dev-secondary hover:underline underline-offset-4'>
                 회원가입
               </Link>
             </li>
