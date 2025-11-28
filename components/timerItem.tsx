f45fd1e681ca4d7a73fa488c962bd63530e8fc65
seonyoungg <tjsduddlfma1@naver.com>
2025-11-29 01:50:15 +0900

    💄 style: 타이머 박스 크기 설정

--- components/timerItem.tsx
+++ components/timerItem.tsx
@@ -5,7 +5,7 @@ interface TimerBoxProps {
 
 export default function TimerItem({ value = 0, label }: TimerBoxProps) {
   return (
-    <div className='p-4 pb-8 border border-dev-primary text-dev-primary rounded-xl bg-linear-to-br from-dev-primary/0 to-dev-primary/20 '>
+    <div className='p-4 pb-8 border border-dev-primary text-dev-primary rounded-xl bg-linear-to-br from-dev-primary/0 to-dev-primary/20 w-[220px]'>
       <p className='text-[154px] font-digital tracking-widest text-center'>{value ? String(value).padStart(2, '0') : '00'}</p>
       <p className='text-sm font-semibold text-center'>{label}</p>
     </div>
